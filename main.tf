5df3943e95c638bed112d53e9a595746fe56d0d7
Tom Lorentsen <toml@giffgaff.co.uk>
2019-03-19 16:31:39 +0000

    Create namespaces with a resource quota

--- main.tf
+++ main.tf
@@ -2,3 +2,31 @@ provider "kubernetes" {
 
 }
 
+variable "namespaces" {
+  type = "list"
+  default = [
+    "test",
+    "staging",
+    "live"
+  ]
+}
+
+resource "kubernetes_namespace" "namespace-test" {
+  count    = "${length(var.namespaces)}"
+  metadata {
+    name = "${var.namespaces[count.index]}"
+  }
+}
+
+resource "kubernetes_resource_quota" "test-resource-quota" {
+  count    = "${length(var.namespaces)}"
+  metadata {
+    name = "${var.namespaces[count.index]}"
+  }
+  spec {
+    hard {
+      pods = 1
+    }
+    scopes = ["BestEffort"]
+  }
+}
