f2ebd8df013f17d7e8964e233527fc8fb6e8ff4d
Tom Lorentsen <toml@giffgaff.co.uk>
2019-03-19 17:11:59 +0000

    Create service account

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "kubernetes_namespace" "namespace-test" {
   }
 }
 
-resource "kubernetes_resource_quota" "test-resource-quota" {
+resource "kubernetes_resource_quota" "cluster-resource-quota" {
   count    = "${length(var.namespaces)}"
   metadata {
     name = "${var.namespaces[count.index]}"
@@ -28,5 +28,40 @@ resource "kubernetes_resource_quota" "test-resource-quota" {
       pods = 1
     }
     scopes = ["BestEffort"]
+  }
+}
+
+resource "kubernetes_limit_range" "cluster-limit-range" {
+  count    = "${length(var.namespaces)}"
+  metadata {
+    name = "${var.namespaces[count.index]}"
+  }
+  spec {
+    limit {
+      type = "Pod"
+      max {
+        cpu = "200m"
+        memory = "1024M"
+      }
+    }
+    limit {
+      type = "PersistentVolumeClaim"
+      min {
+        storage = "24M"
+      }
+    }
+    limit {
+      type = "Container"
+      default {
+        cpu = "50m"
+        memory = "24M"
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_account" "example-service-account" {
+  metadata {
+    name = "example"
   }
 }
